1db2c34bba3894e2d14cbe778731121ffe0ee084
gtoyo <ggabrieltoyo@gmail.com>
2023-10-23 15:01:03 -0300

    Atualização: Campo de busca

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,22 +12,33 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = '';
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
 
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe(listaPensamentos => {
       this.listaPensamentos = listaPensamentos;
     });
   }
 
   carregarMaisPensamentos(): void {
-    this.service.listar(++this.paginaAtual).subscribe( listarPensamentos => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe(listarPensamentos => {
       this.listaPensamentos.push(...listarPensamentos);
       if (!listarPensamentos.length) {
         this.haMaisPensamentos = false;
       }
     });
   }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1;
+
+    this.service.listar(this.paginaAtual, this.filtro).subscribe(listaPensamentos => {
+        this.listaPensamentos = listaPensamentos
+      });
+  }
+
 }
